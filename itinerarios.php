ecbabdcda5a8ea899dfb40aafe46fa3ee39ad3b9
alumno <alumno@alumno-VirtualBox>
2018-05-30 18:11:06 +0200

    Cruceros 2 = Intento ejercicio 1 parte 1

--- itinerarios.php
+++ itinerarios.php
@@ -15,6 +15,48 @@ cabecera("Salidas de los cruceros");
 <th>Fecha</th>
 <th>Acciones</th>
 </tr>
+<?php
+
+$vNoches = isset($_POST['noches']) ? $_POST['noches'] : '';
+$vPuerto = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$vBarco = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$vPais = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$vFechaSalida = isset($_POST['fechaSalida']) ? $_POST['fechaSalida'] : null;
+
+try {    
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+    /*** echo a message saying we have connected ***/
+
+    /*** The SQL SELECT statement ***/
+    $sql = "SELECT * FROM `Cruceros.Itinerarios` JOIN `Cruceros.Puertos` JOIN `Cruceros.Barcos`"
+            . "JOIN `Cruceros.Paises` JOIN `Cruzeros.Salidas` WHERE IdCliente LIKE '%$vIdCliente%'";
+    foreach ($dbh->query($sql) as $row)
+    { ?>
+        <TR>
+            <TD><?php print $row['IdCliente'] ?></TD>
+            <TD><?php print $row['NombreCompany'] ?></TD>
+            <TD><?php print $row['NombreContacto'] ?></TD>
+            <TD><?php print $row['CargoContacto'] ?></TD>
+            <TD><a hhref="verEtapas.php?codigoSalida=<?php echo $row['codigoSalida']; ?>">
+                    <button name="verEtapas" value="verEtapas">Ver Etapas</button>
+                </a>
+               
+            </TD>
+        </TR>
+           <?php }
+
+    /*** close the database connection ***/
+    $dbh = null;
+
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+
+
+?>
+
 <tr>
 <td>7</td>
 <td>Napoles</td>
